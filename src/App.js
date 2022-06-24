a5696f54475ee162a31ab9998d138b35f1a27a42
Shukhrat Gadoev <shukhratgadoev@77projects.com>
2022-06-24 15:59:37 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header'
+import NotesListPage from './pages/NotesListPage';
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <NotesListPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/NotesListPage.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useEffect, useState} from 'react'
+
+const NotesListPage = () => {
+    const [notes, setNotes] = useState([])
+    useEffect(() => {
+
+    },[])
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NotesListPage
